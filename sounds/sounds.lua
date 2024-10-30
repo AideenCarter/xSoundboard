3089ecb8847684666e3acd40def83f9ed6000dc6
Niklas Priebe <70219545+AideenCarter@users.noreply.github.com>
2024-10-30 18:26:16 +0100

    Cleanup
    Alte Datein gelöscht

--- sounds/sounds.lua
+++ sounds/sounds.lua
@@ -28,38 +28,12 @@ name_space.sound_dict["versprecher"] = {path = "Interface\\Addons\\xSoundboard\\
 name_space.sound_dict["wer-trollt-hier"] = {path = "Interface\\Addons\\xSoundboard\\sounds\\ready_wer-trollt-hier.ogg", duration = 3, group = "ready"}
 name_space.sound_dict["scheiße"] = {path = "Interface\\Addons\\xSoundboard\\sounds\\ready_scheiße.ogg", duration = 3, group = "ready"}
 name_space.sound_dict["funkcheck"] = {path = "Interface\\Addons\\xSoundboard\\sounds\\ready_funkcheck.ogg", duration = 2, group = "ready"}
-name_space.sound_dict["bot-play"] = {path = "Interface\\Addons\\xSoundboard\\sounds\\beclouded_bot-play.ogg", duration = 2, group = "beclouded"}
-name_space.sound_dict["dumm"] = {path = "Interface\\Addons\\xSoundboard\\sounds\\beclouded_dumm.ogg", duration = 2, group = "beclouded"}
-name_space.sound_dict["easy"] = {path = "Interface\\Addons\\xSoundboard\\sounds\\beclouded_easy.ogg", duration = 2, group = "beclouded"}
-name_space.sound_dict["freedom"] = {path = "Interface\\Addons\\xSoundboard\\sounds\\beclouded_freedom.ogg", duration = 5, group = "beclouded"}
-name_space.sound_dict["heiler"] = {path = "Interface\\Addons\\xSoundboard\\sounds\\beclouded_heiler.ogg", duration = 2, group = "beclouded"}
-name_space.sound_dict["sterben"] = {path = "Interface\\Addons\\xSoundboard\\sounds\\beclouded_sterben.ogg", duration = 2, group = "beclouded"}
-name_space.sound_dict["try"] = {path = "Interface\\Addons\\xSoundboard\\sounds\\beclouded_try.ogg", duration = 5, group = "beclouded"}
-name_space.sound_dict["wipe"] = {path = "Interface\\Addons\\xSoundboard\\sounds\\beclouded_wipe.ogg", duration = 4, group = "beclouded"}
-name_space.sound_dict["bl"] = {path = "Interface\\Addons\\xSoundboard\\sounds\\kimuh_bl.ogg", duration = 4, group = "kimuh"}
-name_space.sound_dict["computerspiel"] = {path = "Interface\\Addons\\xSoundboard\\sounds\\kimuh_computerspiel.ogg", duration = 4, group = "kimuh"}
-name_space.sound_dict["dummer-nuttenbengel"] = {path = "Interface\\Addons\\xSoundboard\\sounds\\kimuh_dummer-nuttenbengel.ogg", duration = 4, group = "kimuh"}
-name_space.sound_dict["kick-doch"] = {path = "Interface\\Addons\\xSoundboard\\sounds\\kimuh_kick-doch.ogg", duration = 4, group = "kimuh"}
-name_space.sound_dict["nuttenbengel"] = {path = "Interface\\Addons\\xSoundboard\\sounds\\kimuh_nuttenbengel.ogg", duration = 2, group = "kimuh"}
-name_space.sound_dict["blast"] = {path = "Interface\\Addons\\xSoundboard\\sounds\\misc_blast.ogg", duration = 4, group = "misc"}
 name_space.sound_dict["bus"] = {path = "Interface\\Addons\\xSoundboard\\sounds\\misc_bus.ogg", duration = 3, group = "misc"}
-name_space.sound_dict["crippled"] = {path = "Interface\\Addons\\xSoundboard\\sounds\\misc_crippled.ogg", duration = 3, group = "misc"}
-name_space.sound_dict["dogshit"] = {path = "Interface\\Addons\\xSoundboard\\sounds\\misc_dogshit.ogg", duration = 6, group = "misc"}
-name_space.sound_dict["lebenszeit"] = {path = "Interface\\Addons\\xSoundboard\\sounds\\misc_lebenszeit.ogg", duration = 3, group = "misc"}
-name_space.sound_dict["arkanblast"] = {path = "Interface\\Addons\\xSoundboard\\sounds\\nephis_arkanblast.ogg", duration = 4, group = "nephis"}
-name_space.sound_dict["basics"] = {path = "Interface\\Addons\\xSoundboard\\sounds\\nephis_basics.ogg", duration = 6, group = "nephis"}
-name_space.sound_dict["dmg-stop"] = {path = "Interface\\Addons\\xSoundboard\\sounds\\nephis_dmg-stop.ogg", duration = 2, group = "nephis"}
-name_space.sound_dict["drecks"] = {path = "Interface\\Addons\\xSoundboard\\sounds\\nephis_drecks.ogg", duration = 1, group = "nephis"}
-name_space.sound_dict["herzkasper"] = {path = "Interface\\Addons\\xSoundboard\\sounds\\nephis_herzkasper.ogg", duration = 3, group = "nephis"}
 name_space.sound_dict["immer-die-gleichen"] = {path = "Interface\\Addons\\xSoundboard\\sounds\\nephis_immer-die-gleichen.ogg", duration = 2, group = "nephis"}
-name_space.sound_dict["konzentriert-euch"] = {path = "Interface\\Addons\\xSoundboard\\sounds\\nephis_konzentriert-euch.ogg", duration = 3, group = "nephis"}
-name_space.sound_dict["kotzeritis"] = {path = "Interface\\Addons\\xSoundboard\\sounds\\nephis_kotzeritis.ogg", duration = 3, group = "nephis"}
 name_space.sound_dict["macht-euch-das-spa"] = {path = "Interface\\Addons\\xSoundboard\\sounds\\nephis_macht-euch-das-spa.ogg", duration = 7, group = "nephis"}
 name_space.sound_dict["piss"] = {path = "Interface\\Addons\\xSoundboard\\sounds\\nephis_piss.ogg", duration = 1, group = "nephis"}
 name_space.sound_dict["reinhalterinio"] = {path = "Interface\\Addons\\xSoundboard\\sounds\\nephis_reinhalterinio.ogg", duration = 2, group = "nephis"}
-name_space.sound_dict["rezz-ihn"] = {path = "Interface\\Addons\\xSoundboard\\sounds\\nephis_rezz-ihn.ogg", duration = 4, group = "nephis"}
-name_space.sound_dict["schweine"] = {path = "Interface\\Addons\\xSoundboard\\sounds\\nephis_schweine.ogg", duration = 2, group = "nephis"}
-name_space.sound_dict["versteht-es-nicht"] = {path = "Interface\\Addons\\xSoundboard\\sounds\\nephis_versteht-es-nicht.ogg", duration = 7, group = "nephis"}
-name_space.sound_dict["mossad"] = {path = "Interface\\Addons\\xSoundboard\\sounds\\philip_mossad.ogg", duration = 5, group = "philip"}
+
+
 
 
